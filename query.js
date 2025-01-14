a78900ea5231672b190934323a511b35f6454c0e
MOHAMMAD SAQLAIN <32585833+mrrootsec@users.noreply.github.com>
2025-01-14 21:22:43 +0530

    Update query.js

--- query.js
+++ query.js
@@ -2,7 +2,7 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type User {
-    user_id: ID
+    user_id: String
     name: String
     email: String
     phone: String
@@ -12,24 +12,34 @@ const typeDefs = gql`
   }
 
   type Event {
-    event_id: ID
+    event_id: String
     name: String
     description: String
-    ownerId: String
+    createdAt: String
+  }
+
+  type RSVPedUser {
+    name: String
+    email: String
+    phone: String
   }
 
   type Query {
     users: [User]
-    user(user_id: ID!): User
-    event(event_id: ID!): Event
+    user(user_id: String!): User
+    events: [Event]
+    event(event_id: String!): Event
+    rsvpedUsers(event_id: String!): [RSVPedUser]
+    healthCheck(input: String!): String
   }
 
   type Mutation {
     register(name: String!, email: String!, phone: String!, password: String!): String
     login(email: String!, password: String!): String
     editProfile(name: String, phone: String, email: String, isAdmin: Boolean): String
-    editEvent(event_id: ID!, name: String!, description: String!): String
-  }
+    editEvent(event_id: String!, name: String, description: String): String
+    rsvpEvent(event_id: String!): String
+    cancelRsvp(event_id: String!, user_id: String!): String  }
 `;
 
 module.exports = { typeDefs };
